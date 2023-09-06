0898432e79b665bad64616b6ae65c9aa273b0e62
muneeswaran7 <144212567+muneeswaran7@users.noreply.github.com>
2023-09-06 19:33:46 +0530

    Update main.py

--- challenge unit-1/main.py
+++ challenge unit-1/main.py
@@ -1,8 +1,5 @@
-if num < 0:
-   print("Sorry, factorial does not exist for negative numbers")
-elif num == 0:
-   print("The factorial of 0 is 1")
-else:
-   for i in range(1,num + 1):
-       factorial = factorial*i
-   print("The factorial of",num,"is",factorial)
+n=int(input("enter a no:"))
+factorial=1
+for i in range (1,n+1):
+	factorial=factorial*i
+print(factorial
