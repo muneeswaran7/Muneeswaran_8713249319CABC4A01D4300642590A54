c186b6972552264cb633fe1d0957ba7a80d39a8e
muneeswaran7 <144212567+muneeswaran7@users.noreply.github.com>
2023-09-06 19:39:54 +0530

    Update leap year.py

--- challenge unit-1/leap year.py
+++ challenge unit-1/leap year.py
@@ -1,12 +1,4 @@
-if (year % 400 == 0) and (year % 100 == 0):
-    print("{0} is a leap year".format(year))
-
-# not divided by 100 means not a century year
-# year divided by 4 is a leap year
-elif (year % 4 ==0) and (year % 100 != 0):
-    print("{0} is a leap year".format(year))
-
-# if not divided by both 400 (century year) and 4 (not century year)
-# year is not leap year
-else:
-    print("{0} is not a leap year".format(year))
+year=int(input("Enter a no:") ) 
+if(year%4==0 and year%100! =0) or(year%400==0) :
+    print(year,"is a leap year")
+print(year,"is not a leap year") 
