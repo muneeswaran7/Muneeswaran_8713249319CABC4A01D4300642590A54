2d7f63db819fe24bcb35f386387c71f16cc74526
muneeswaran7 <144212567+muneeswaran7@users.noreply.github.com>
2023-09-06 17:51:02 +0530

    Add files via upload

--- challenge unit-1/main.py
+++ challenge unit-1/main.py
@@ -1,4 +1,8 @@
-year=int(input("Enter a year:"))
-if(year%4==0 and year % 100!=0)or(year%400==0):
-  print(year,"is a leap year")
-  print(year,"is not leap year")
+if num < 0:
+   print("Sorry, factorial does not exist for negative numbers")
+elif num == 0:
+   print("The factorial of 0 is 1")
+else:
+   for i in range(1,num + 1):
+       factorial = factorial*i
+   print("The factorial of",num,"is",factorial)
