7d0c2cb50735557aca386bcd4837278ea2686d84
Muneeswaran <144212567+muneeswaran7@users.noreply.github.com>
2023-09-23 20:56:39 +0530

    student.py

--- challenge unit-3/student.py
+++ challenge unit-3/student.py
@@ -1,24 +1,16 @@
-def sort_students(student_list):
-    sorted_students = sorted(student_list, key=lambda x: x.cgpa, reverse=True)
-    return sorted_students
-
-# Define a Student class for testing
 class Student:
-    def _init_(self, name, roll_number, cgpa):
-        self.name = name
-        self.roll_number = roll_number
-        self.cgpa = cgpa
 
-# Test the function with a list of student objects
-students = [
-    Student("Alice", "A001", 3.9),
-    Student("Bob", "B002", 3.7),
-    Student("Charlie", "C003", 3.5),
-    Student("David", "D004", 4.0),
-]
+  def __init__(self,name,roll_number,cgpa):
+    self.name = name
+    self.roll_number = roll_number
+    self.cgpa = cgpa
 
+def sort_students(student_list):
+  sorted_students = sorted(student_list,key=lambda student: student.cgpa,reverse=True)
+  return sorted_students
+
+students=[Student("muneeswaran","Y123",7.8),Student("Srinidhi","Y124",8.9),Student("Sam","y125",9.1),Student("Madhu","Y126",9.9)]
 sorted_students = sort_students(students)
 
-# Print the sorted list
 for student in sorted_students:
-    print(f"Name: {student.name}, Roll Number: {student.roll_number}, CGPA: {student.cgpa}")
+  print("Name: {},Roll Number: {},CGPA: {}".format(student.name,student.roll_number,student.cgpa))
